e25598356ea259a85c82885e5a90b9a6a1dc805d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-09-28 02:42:46 +0000

    update to latest http_dynamic_server.go

--- http_dynamic_server.go
+++ http_dynamic_server.go
@@ -1,32 +1,39 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"compress/flate"
 	"fmt"
 	"io"
+	"io/fs"
 	"net/http"
 	"os"
 	"os/signal"
+	"path"
 	"path/filepath"
 	"strings"
 	"syscall"
 	"time"
 )
 
+// ServerConfig represents all files known to the server
+type ServerConfig struct {
+	Handlers  []Handler
+	CleanURLS bool
+	Port      int
+}
+
+type HandlerFunc = func(w http.ResponseWriter, r *http.Request)
+
 // Handler represents one or more urls and their content
 type Handler interface {
 	// returns a handler for this url
 	// if nil, doesn't handle this url
-	Get(url string) func(w http.ResponseWriter, r *http.Request)
+	Get(url string) HandlerFunc
 	// get all urls handled by this Handler
 	// useful for e.g. saving a static copy to disk
 	URLS() []string
-}
-
-type FileHandler struct {
-	// Path on disk for this file
-	Path string
-	// list of urls that this file matches
-	URL []string
 }
 
 // FileWriter implements http.ResponseWriter interface for writing to a file
@@ -46,18 +53,39 @@ func (w *FileWriter) WriteHeader(statusCode int) {
 	// no-op
 }
 
-func (h *FileHandler) Get(url string) func(w http.ResponseWriter, r *http.Request) {
+type FileHandler struct {
+	// Path on disk for this file
+	Path string
+	// list of urls that this file matches
+	URL []string
+}
+
+func serveFile(w http.ResponseWriter, r *http.Request, path string) {
+	if r == nil {
+		d := readFileMust(path)
+		_, err := w.Write(d)
+		must(err)
+		return
+	}
+	http.ServeFile(w, r, path)
+}
+
+func serveContent(w http.ResponseWriter, r *http.Request, d []byte, path string) {
+	if r == nil {
+		_, err := w.Write(d)
+		must(err)
+		return
+	}
+	content := bytes.NewReader(d)
+	http.ServeContent(w, r, path, time.Now(), content)
+}
+
+func (h *FileHandler) Get(url string) HandlerFunc {
 	for _, u := range h.URL {
 		// urls are case-insensitive
 		if strings.EqualFold(u, url) {
 			return func(w http.ResponseWriter, r *http.Request) {
-				if r == nil {
-					d := readFileMust(h.Path)
-					_, err := w.Write(d)
-					must(err)
-				} else {
-					http.ServeFile(w, r, h.Path)
-				}
+				serveFile(w, r, h.Path)
 			}
 		}
 	}
@@ -79,6 +107,63 @@ func NewFileHandler(path string, url string, urls ...string) *FileHandler {
 	return res
 }
 
+type DirHandler struct {
+	Dir       string
+	URLPrefix string
+
+	URL   []string
+	paths []string // same order as URL
+}
+
+func (h *DirHandler) Get(url string) func(w http.ResponseWriter, r *http.Request) {
+	for i, u := range h.URL {
+		// urls are case-insensitive
+		if strings.EqualFold(u, url) {
+			return func(w http.ResponseWriter, r *http.Request) {
+				serveFile(w, r, h.paths[i])
+			}
+		}
+	}
+	return nil
+}
+
+func (h *DirHandler) URLS() []string {
+	return h.URL
+}
+
+func getURLSForFiles(startDir string, urlPrefix string, acceptFile func(string) bool) (urls []string, paths []string) {
+	filepath.WalkDir(startDir, func(filePath string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+		if !d.Type().IsRegular() {
+			return nil
+		}
+		if acceptFile != nil && !acceptFile(filePath) {
+			return nil
+		}
+		dir := strings.TrimPrefix(filePath, startDir)
+		dir = filepath.ToSlash(dir)
+		dir = strings.TrimPrefix(dir, "/")
+		uri := path.Join(urlPrefix, dir)
+		//logf("getURLSForFiles: dir: '%s'\n", dir)
+		urls = append(urls, uri)
+		paths = append(paths, filePath)
+		return nil
+	})
+	return
+}
+
+func NewDirHandler(dir string, urlPrefix string, acceptFile func(string) bool) *DirHandler {
+	urls, paths := getURLSForFiles(dir, urlPrefix, acceptFile)
+	return &DirHandler{
+		Dir:       dir,
+		URLPrefix: urlPrefix,
+		URL:       urls,
+		paths:     paths,
+	}
+}
+
 type DynamicHandler struct {
 	matches func(string) func(http.ResponseWriter, *http.Request)
 	urls    func() []string
@@ -96,6 +181,48 @@ func NewDynamicHandler(matches func(string) func(http.ResponseWriter, *http.Requ
 	return &DynamicHandler{
 		matches: matches,
 		urls:    urls,
+	}
+}
+
+type InMemoryFilesHandler struct {
+	files map[string][]byte
+}
+
+func (h *InMemoryFilesHandler) Get(uri string) func(http.ResponseWriter, *http.Request) {
+	for path, d := range h.files {
+		if strings.EqualFold(path, uri) {
+			return func(w http.ResponseWriter, r *http.Request) {
+				serveContent(w, r, d, uri)
+			}
+		}
+	}
+	return nil
+}
+
+func (h *InMemoryFilesHandler) URLS() []string {
+	var urls []string
+	for path := range h.files {
+		urls = append(urls, path)
+	}
+	return urls
+}
+
+func NewInMemoryFilesHandler(files map[string][]byte) *InMemoryFilesHandler {
+	for path, d := range files {
+		newp := strings.Replace(path, "\\", "/", -1)
+		if !strings.HasPrefix(newp, "/") {
+			newp = "/" + newp
+		}
+		newp = "/" + path
+		if path == newp {
+			continue
+		}
+		files[newp] = d
+		delete(files, path)
+	}
+
+	return &InMemoryFilesHandler{
+		files: files,
 	}
 }
 
@@ -121,10 +248,53 @@ func WriteServerFilesToDir(dir string, handlers []Handler) {
 	}
 }
 
-// ServerConfig represents all files known to the server
-type ServerConfig struct {
-	Handlers  []Handler
-	CleanURLS bool
+func zipWriteContent(zw *zip.Writer, files map[string][]byte) error {
+	for name, data := range files {
+		fw, err := zw.Create(name)
+		if err != nil {
+			return err
+		}
+		_, err = fw.Write(data)
+		if err != nil {
+			return err
+		}
+	}
+	return zw.Close()
+}
+
+func zipCreateFromContent(files map[string][]byte) ([]byte, error) {
+	var buf bytes.Buffer
+	zw := zip.NewWriter(&buf)
+	zw.RegisterCompressor(zip.Deflate, func(out io.Writer) (io.WriteCloser, error) {
+		return flate.NewWriter(out, flate.BestCompression)
+	})
+	err := zipWriteContent(zw, files)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func WriteServerFilesToZip(handlers []Handler) ([]byte, error) {
+	files := map[string][]byte{}
+	for _, h := range handlers {
+		urls := h.URLS()
+		for _, uri := range urls {
+			path := strings.TrimPrefix(uri, "/")
+			var buf bytes.Buffer
+			fw := &FileWriter{
+				w: &buf,
+			}
+			serve := h.Get(uri)
+			panicIf(serve == nil, "must have a handler for '%s'", uri)
+			serve(fw, nil)
+			d := buf.Bytes()
+			files[path] = d
+			sizeStr := formatSize(int64(len(d)))
+			logf(ctx(), "WriteServerFilesZip: '%s' of size %s\n", path, sizeStr)
+		}
+	}
+	return zipCreateFromContent(files)
 }
 
 // returns function that will wait for SIGTERM signal (e.g. Ctrl-C) and
@@ -132,6 +302,9 @@ type ServerConfig struct {
 func StartServer(server *ServerConfig) func() {
 	panicIf(server == nil, "must provide files")
 	httpPort := 8080
+	if server.Port != 0 {
+		httpPort = server.Port
+	}
 	httpAddr := fmt.Sprintf(":%d", httpPort)
 	if isWindows() {
 		httpAddr = "localhost" + httpAddr
@@ -139,8 +312,8 @@ func StartServer(server *ServerConfig) func() {
 	mux := &http.ServeMux{}
 	handleAll := func(w http.ResponseWriter, r *http.Request) {
 		uri := r.URL.Path
-		if uri == "/" {
-			uri = "/index.html"
+		if strings.HasSuffix(uri, "/") {
+			uri += "index.html"
 		}
 		trySend := func(uri string) bool {
 			for _, h := range server.Handlers {
